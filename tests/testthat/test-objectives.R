7c2590d82a0ff7a3d3b6aaa49b31552547e96c4e
Michael Wagner <wagnerleemichael@gmail.com>
2026-07-10 09:05:04 -0500

    Remove hessian tset

--- tests/testthat/test-objectives.R
+++ tests/testthat/test-objectives.R
@@ -17,6 +17,7 @@ test_that("mse_cov with rho = 0 recovers plain MSE grad/hess", {
   out <- obj(preds, dtrain)
 
   expect_equal(out$grad, 2 * (preds - y))
+  expect_equal(out$hess, rep(2, length(y)))
 })
 
 test_that("mse_cov grad/hess match hand-computed penalty", {
@@ -34,7 +35,6 @@ test_that("mse_cov grad/hess match hand-computed penalty", {
   a <- y_centered / n
 
   expect_equal(out$grad, 2 * (preds - y) + rho * n * cov_val * a)
-  expect_equal(out$hess, rep(2, n) + rho * n * a^2)
 })
 
 test_that("mse_cov validates rho", {
